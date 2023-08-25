1be190087d5b80c1c913b881bfda85dda2e5c3ea
Kirpi <kirpicheva89@yandex.ru>
2023-08-26 02:43:37 +0300

    Диплом «исследуй со мной» 1.0

---
+++ stats/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
@@ -0,0 +1,32 @@
+package ru.practicum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "hits")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class EndpointHit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "app")
+    private String app;
+
+    @Column(name = "uri")
+    private String uri;
+
+    @Column(name = "ip")
+    private String ip;
+
+    @Column(name = "timestamp")
+    private LocalDateTime timestamp;
+
+}
