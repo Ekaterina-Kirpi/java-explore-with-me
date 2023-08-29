2e0de7be39ae11744fe70daf75031d4353cd8c2e
Kirpi <kirpicheva89@yandex.ru>
2023-08-29 10:04:02 +0300

    1

---
+++ main/src/main/java/EwmApp.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class EwmApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EwmApp.class);
+    }
+}
